cdb015cf13875af556571f60a9a981f6a44ea142
Alvaro Cubiella <alvarocubiella@gmail.com>
2025-04-09 10:13:47 -0300

    Actualizado 2025

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ import json
 import time
 import threading
 import requests
-import ast          # lo uso para convertir una cadena de estaciones (de la API) en diccionario
+# lo uso para convertir una cadena de estaciones (de la API) en diccionario
+import ast
 import schedule
 
 import telebot
@@ -15,12 +16,12 @@ from telebot.types import InlineKeyboardMarkup
 from telebot.types import InlineKeyboardButton
 import schedule
 
-from flask import Flask, request                # Para crear nuestro servidor web propio LOCAL
-from pyngrok import ngrok, conf                 # Creo el nexo entre nuestro servidor web local y el bot de telegram
+# Para crear nuestro servidor web propio LOCAL
+from flask import Flask, request
 from waitress import serve
 
-from modulos import sst_server
-from modulos.utils import *
+from App.modulos import sst_server
+from App.modulos.utils import *
 
 __version__ = "V 1 0"
 __date__ = "2023/12/12"
@@ -35,12 +36,12 @@ user_admin = ()
 
 user_id_admin = (
     5421431478,
-    )
+)
 user_admin = (
     'AlvaroCubiella',
-    )
+)
 
-w_list=(
+w_list = (
     'AlvaroCubiella',
     'EmmanuelZel',
     'NoeManik4',
@@ -48,31 +49,31 @@ w_list=(
     'Jorge_fabrego',
     'Harold_Fenco',
     'gnmolinari',
-    )
+)
 
 # Lista de comandos para usuarios
 w_cmd_lit = (
     'start',
     'info',
     'help',
-    )
+)
 
 # Establezco a partir de que nivel se imprimen los mensajes y formato
 logging.basicConfig(level=logging.INFO,
                     format="%(asctime)s -  %(processName)s - %(levelname)s - %(message)s",
-                    filename= 'Telegram_BOT.log',
-                    filemode= 'a')
+                    filename='Telegram_BOT.log',
+                    filemode='a')
 
 file = f"{os.getcwd()}{os.sep}T_Bot.json"
 
 try:
-    with open(file, 'r') as f:  
-        cfg = json.load(f)  
+    with open(file, 'r') as f:
+        cfg = json.load(f)
 except:
     logging.critical(f"No se pudo cargar el archivo de configuracion {file}")
     sys.exit(0)
 
-#Estacion
+# Estacion
 estacion = cfg['Estacion']['Nombre']
 sensor = cfg['Estacion']['Sensor']
 
@@ -85,25 +86,31 @@ carpeta = cfg['FTP']['Carpeta']
 ftp = sst_server.SST_Servidor(
     servidor=servidor,
     usuario=usuario,
-    psw = passw,
-    root= carpeta
+    psw=passw,
+    root=carpeta
 )
 
 # Iniciamos el servidor web local con flask
 web_server = Flask(__name__)
 
 # Gestiona las peticiones POST enviadas al servidor web
-@web_server.route("/", methods = ['POST'])
+
+
+@web_server.route("/", methods=['POST'])
 def webhook():
     # si el POST recibido es un json
     if request.headers.get("content-type") == "application/json":
-        update = telebot.types.Update.de_json(request.stream.read().decode("utf-8"))
+        update = telebot.types.Update.de_json(
+            request.stream.read().decode("utf-8"))
         bot.process_new_updates([update])
         return "OK", 200
+
 
 bot = telebot.TeleBot(TOKEN_TELEGRAM_BOT)
 
 # Decorador para verificar si el usuario está autorizado
+
+
 def usuario_autorizado(func):
     def verificar_usuario(message):
         user_id = message.from_user.id
@@ -112,15 +119,20 @@ def usuario_autorizado(func):
             func(message)
         else:
             if message.chat.username is None:
-                bot.reply_to(message, f"Debe establecer un nombre de usuario Telegram para continuar", parse_mode="HTML")
+                bot.reply_to(
+                    message, f"Debe establecer un nombre de usuario Telegram para continuar", parse_mode="HTML")
             else:
-                bot.reply_to(message, "Lo siento, no estás autorizado para ejecutar este comando.")
-                logging.error(f"El usuario {message.chat.username} con ID: {message.from_user.id} fue rechazado")
+                bot.reply_to(
+                    message, "Lo siento, no estás autorizado para ejecutar este comando.")
+                logging.error(
+                    f"El usuario {message.chat.username} con ID: {message.from_user.id} fue rechazado")
                 mensaje = f"Solicitud del usuario {message.chat.username} ID: {message.from_user.id}"
                 bot.send_message(5421431478, mensaje)
     return verificar_usuario
 
 # Decorador para verificar si el usuario es el administrador
+
+
 def is_admin(func):
     def verificar_admin(message):
         user_id = message.from_user.id
@@ -131,10 +143,13 @@ def is_admin(func):
         else:
             authorized = False
             if func.__name__ != 'cmd_botones':
-                bot.send_message(message.chat.id, "Lo siento, no estás autorizado para ejecutar este comando.")
-                logging.error(f"El usuario {message.chat.username} con ID: {message.from_user.id} fue rechazado")
+                bot.send_message(
+                    message.chat.id, "Lo siento, no estás autorizado para ejecutar este comando.")
+                logging.error(
+                    f"El usuario {message.chat.username} con ID: {message.from_user.id} fue rechazado")
             func(message, authorized)
     return verificar_admin
+
 
 @bot.message_handler(commands=["start"])
 @usuario_autorizado
@@ -142,15 +157,18 @@ def cmd_start(message):
     cadena = f'Bienvenido <b>{message.chat.username}</b>\n'
     cadena += f'Soy SST_Bot, en que puedo ayudarlo?' + '\n\n'
     cadena += f'Menu de opciones' + '\n'
-    #bot.send_message(message.chat.id, cadena, parse_mode="HTML")
     time.sleep(0.25)
     """ Muestra un mensaje con los botonos de comandos inline (dentro del chat) """
-    markup = InlineKeyboardMarkup(row_width=2)              # Botones por linea por defecto 3
-    btn_estaciones = InlineKeyboardButton("Ver Estaciones", callback_data="estaciones")
+    markup = InlineKeyboardMarkup(
+        row_width=2)              # Botones por linea por defecto 3
+    btn_estaciones = InlineKeyboardButton(
+        "Ver Estaciones", callback_data="estaciones")
     btn_ayuda = InlineKeyboardButton("Información", callback_data="info")
     btn_cerrar = InlineKeyboardButton("Cerrar", callback_data="cerrar")
     markup.add(btn_estaciones, btn_ayuda, btn_cerrar)
-    bot.send_message(message.chat.id, cadena, parse_mode="HTML", reply_markup=markup)
+    bot.send_message(message.chat.id, cadena,
+                     parse_mode="HTML", reply_markup=markup)
+
 
 @bot.message_handler(commands=["help"])
 @usuario_autorizado
@@ -165,9 +183,9 @@ def cmd_help(message):
         for comando in comandos:
             if comando.command in w_cmd_lit:
                 cadena += f"*/{comando.command}*\tDescripción: {comando.description}\n"
-    
-    
+
     bot.send_message(message.chat.id, cadena, parse_mode="MarkdownV2")
+
 
 @bot.message_handler(commands=["info"])
 @usuario_autorizado
@@ -175,31 +193,40 @@ def cmd_info(message):
     cadena = __GetInfo()
     bot.send_message(message.chat.id, cadena, parse_mode="MarkdownV2")
 
+
 @bot.message_handler(commands=["alta"])
 @is_admin
 def cmd_alta(message, authorized):
     if authorized:
         markup = ForceReply()
-        msg = bot.send_message(message.chat.id, "Nombre del nuevo usuario:", reply_markup = markup)
+        msg = bot.send_message(
+            message.chat.id, "Nombre del nuevo usuario:", reply_markup=markup)
         bot.register_next_step_handler(msg, nuevo_usuario)
+
 
 @bot.message_handler(commands=["baja"])
 @is_admin
 def cmd_baja(message, authorized):
     if authorized:
         markup = ForceReply()
-        msg = bot.send_message(message.chat.id, "Nombre del usuario a eliminar:", reply_markup = markup)
+        msg = bot.send_message(
+            message.chat.id, "Nombre del usuario a eliminar:", reply_markup=markup)
         bot.register_next_step_handler(msg, baja_usuario)
+
 
 def nuevo_usuario(message):
     usuario = message.text
     logging.info(f"Se ah eliminado el usuario {usuario}")
-    bot.send_message(message.chat.id, f'Usuario *{usuario}* eliminado', parse_mode="MarkdownV2")
+    bot.send_message(
+        message.chat.id, f'Usuario *{usuario}* eliminado', parse_mode="MarkdownV2")
+
 
 def baja_usuario(message):
     usuario = message.text
     logging.info(f"Se ah dado de alta un nuevo usuario: {usuario}")
-    bot.send_message(message.chat.id, f'Usuario *{usuario}* eliminado', parse_mode="MarkdownV2")
+    bot.send_message(
+        message.chat.id, f'Usuario *{usuario}* eliminado', parse_mode="MarkdownV2")
+
 
 @bot.message_handler(commands=["bot_estaciones"])
 @is_admin
@@ -216,7 +243,8 @@ def cmd_botones(message, authorized):
     estaciones = ftp_file_json.json()
     estaciones['estaciones'] = ast.literal_eval(estaciones['estaciones'])
     estaciones = estaciones['estaciones']
-    markup = InlineKeyboardMarkup(row_width=2)              # Botones por linea por defecto 3
+    # Botones por linea por defecto 3
+    markup = InlineKeyboardMarkup(row_width=2)
     # Crear botones dinámicamente según la información del archivo JSON
     for key, value in estaciones.items():
         callback_data = f"{key}"
@@ -224,20 +252,25 @@ def cmd_botones(message, authorized):
         markup.add(button)
     button = InlineKeyboardButton("Cerrar", callback_data="cerrar")
     markup.add(button)
-    bot.send_message(message.chat.id, "🌡Selecciona una Estacion de monitoreo", reply_markup=markup)
+    bot.send_message(
+        message.chat.id, "🌡Selecciona una Estacion de monitoreo", reply_markup=markup)
+
 
 @bot.message_handler(commands=["bot_Accion"])
 @usuario_autorizado
 def cmd_btn_aciones(message):
     """ Muestra un mensaje con los botonos de comandos inline (dentro del chat) """
-    markup = InlineKeyboardMarkup(row_width=2)              # Botones por linea por defecto 3
-    btn_estaciones = InlineKeyboardButton("Ver Estaciones", callback_data="estaciones")
+    markup = InlineKeyboardMarkup(
+        row_width=2)              # Botones por linea por defecto 3
+    btn_estaciones = InlineKeyboardButton(
+        "Ver Estaciones", callback_data="estaciones")
     btn_ayuda = InlineKeyboardButton("Ayuda", callback_data="ayuda")
     btn_cerrar = InlineKeyboardButton("Cerrar", callback_data="cerrar")
     markup.add(btn_estaciones, btn_ayuda, btn_cerrar)
     bot.send_message(message.chat.id, "Menu de opciones", reply_markup=markup)
 
-@bot.callback_query_handler(func= lambda x: True)
+
+@bot.callback_query_handler(func=lambda x: True)
 def repuesta_botones_inline(call):
     """Gestiono las acciones de los botones inline"""
     global estaciones, estacion_seleccionada
@@ -249,19 +282,20 @@ def repuesta_botones_inline(call):
         mostrar_info_estacion(chat_id, message_id, username)
     elif call.data == "estaciones":
         estacion_seleccionada = None
-        bot.delete_message(chat_id, message_id) 
-        cmd_botones(call.message)    
+        bot.delete_message(chat_id, message_id)
+        cmd_botones(call.message)
     elif call.data == "cerrar":
-        bot.delete_message(chat_id, message_id) 
+        bot.delete_message(chat_id, message_id)
         cmd_start(call.message)
     elif call.data == "volver":
         estacion_seleccionada = None
-        bot.delete_message(chat_id, message_id) 
+        bot.delete_message(chat_id, message_id)
         cmd_botones(call.message)
     elif call.data == 'info':
         estacion_seleccionada = None
-        bot.delete_message(chat_id, message_id) 
-        cmd_info(call.message)    
+        bot.delete_message(chat_id, message_id)
+        cmd_info(call.message)
+
 
 def mostrar_info_estacion(chat_id, message_id, username):
     global estacion_seleccionada
@@ -280,38 +314,47 @@ def mostrar_info_estacion(chat_id, message_id, username):
         valor = f"<b>Ultimo registro:</b> {estacion['info']['fecha']} {estacion['info']['hora']}\n<b>Valor:</b> {estacion['info']['dato']}\n"
         mensaje += valor
         mensaje += f"<b>Coordenadas:</b> [{latitud}, {longitud}]\n"
-        # <a href="https://www.Tecnonucleous.com/">Tecnonucleous</a>
         mensaje += f" <a href='https://www.google.com/maps?q={estacion['Posicion'][0]},{estacion['Posicion'][1]}'>Coordenadas</a>"
         # Botón para volver al menú de estaciones
-        markup = InlineKeyboardMarkup().add(InlineKeyboardButton("Volver a la lista de estaciones", callback_data="volver"))
-        # <a href="https://www.Tecnonucleous.com/">Tecnonucleous</a>
-        bot.edit_message_text(chat_id=chat_id, message_id=message_id, text=mensaje, parse_mode="HTML", reply_markup=markup)
-        logging.info(f"Consulta realizada sobre la estacion {estacion_seleccionada} por {username}")
+        markup = InlineKeyboardMarkup().add(InlineKeyboardButton(
+            "Volver a la lista de estaciones", callback_data="volver"))
+        bot.edit_message_text(chat_id=chat_id, message_id=message_id,
+                              text=mensaje, parse_mode="HTML", reply_markup=markup)
+        logging.info(
+            f"Consulta realizada sobre la estacion {estacion_seleccionada} por {username}")
     else:
-        bot.send_message(chat_id, "Información no encontrada para esta estación.")
-    
+        bot.send_message(
+            chat_id, "Información no encontrada para esta estación.")
+
+
 @bot.message_handler(content_types=["text"])
 @usuario_autorizado
 def bot_mensajes_texto(message):
     if message.text.startswith("/"):
         bot.send_message(message.chat.id, "comando invalido")
     else:
-        bot.send_message(message.chat.id, "Consulte los comandos disponibles para interactuar \help")
+        bot.send_message(
+            message.chat.id, "Consulte los comandos disponibles para interactuar \help")
+
 
 def GetFolderJSON(archivo):
     with open(archivo, 'wb') as local_file:
         ftp.retrbinary('RETR ' + archivo, local_file.write)
 
+
 def __GetInfo():
     cadena = f'*SST Bot*\n'
     cadena += f"Aplicacion para comunicacion con las estaciones de monitoreo de la red de sensoramiento de temperatura superficial del mar\n"
-    cadena += f'*Desarrollador:* {__author__}' + '\n' 
+    cadena += f'*Desarrollador:* {__author__}' + '\n'
     cadena += f'*Version:*{__version__}' + '\n'
     cadena += f'*Update:* {__date__}' + '\n'
     return cadena
 
-# Función para enviar el mensaje
-def enviar_mensaje(access = 'users'):
+
+
+
+def enviar_mensaje(access='users'):
+    # Función para enviar el mensaje
     url = "https://sst-api-ac6y.onrender.com/actualizar-ftp_admins"
     ftp_file_json = requests.get(url)
     if ftp_file_json.status_code != 200:
@@ -319,7 +362,7 @@ def enviar_mensaje(access = 'users'):
     estaciones = ftp_file_json.json()
     estaciones['estaciones'] = ast.literal_eval(estaciones['estaciones'])
     estaciones = estaciones['estaciones']
-    
+
     # Recorro estacion por estacion
     for i in estaciones:
         # Mensaje a enviar
@@ -339,24 +382,30 @@ def enviar_mensaje(access = 'users'):
             # Enviar el mensaje al chat específico
             bot.send_message(chat_id, mensaje, parse_mode="HTML")
 
+
 def enviar_mensaje_admins():
-    enviar_mensaje(access = 'admin')
+    enviar_mensaje(access='admin')
 
 # Función para programar el mensaje diario
+
+
 def programar_mensaje():
-    #schedule.every().day.at("08:00").do(cmd_start)
     schedule.every().day.at("08:00").do(enviar_mensaje)
     schedule.every().day.at("20:00").do(enviar_mensaje_admins)
     schedule.every(10).minutes.do(enviar_mensaje)
+
 
 # Ejecutar la programación del mensaje al inicio
 programar_mensaje()
 
 # Función para ejecutar el schedule
+
+
 def ejecutar_schedule():
     while True:
         schedule.run_pending()
         time.sleep(60)
+
 
 # Ejecutar el schedule en un hilo aparte
 schedule_thread = threading.Thread(target=ejecutar_schedule)
@@ -367,33 +416,8 @@ if __name__ == "__main__":
         telebot.types.BotCommand("/start", "Inicia el BOT"),
         telebot.types.BotCommand("/help", "Lista los comandos"),
         telebot.types.BotCommand("/info", "Información del bot"),
-        ])
+    ])
     time.sleep(1)
     print(f"BOT SST Telegram iniciado version {__version__}")
-    conf.get_default().config_path = ".//config_ngrok.yml"
-    # REGIONES DISPONIBLES:
-    # us - United States
-    # eu - Europe
-    # ap - Asia
-    # sa - South America (Sao Paulo)
-    conf.get_default().region = "sa" 
-    # creamos el archivo de credenciales de la API ngrok
-    ngrok.set_auth_token(NGORK_TOKEN)
-    # Creamos un TUNEL https en el puerto 5000 para conectar ngrok con flask
-    ngrok_tunel = ngrok.connect(5000, bind_tls = True)
-    # url del tunel https creado
-    ngrok_url = ngrok_tunel.public_url
-    # eliminamos el webhook
-    bot.remove_webhook()
-    # hacemos una pausa para que lo termine de eliminar y no genere conflictos
-    time.sleep(1)
-    # definimos el nuevo webhook
-    bot.set_webhook(url=ngrok_url)
-    # Finalmente iniciamos el servidor
-    serve(web_server, host="0.0.0.0", port = 5000)
-    programar_mensaje()
-
-
-
-
-    #bot.infinity_polling(none_stop=True)
+    # Iniciar el servidor Flask
+    serve(web_server, host="0.0.0.0", port=int(os.getenv('PORT', 5000)))
